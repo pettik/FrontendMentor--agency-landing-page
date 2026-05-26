bb24269eb7d358e183a5bea5a2c0e4fa3d1a81e0
PetrB <petrbednarski@gmail.com>
2026-05-26 12:39:01 +0200

    js added

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+const hamburger = document.querySelector(".nav__hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  console.log("click");
+  mobileMenu.classList.toggle("is-open");
+
+  const isOpen = mobileMenu.classList.contains("is-open");
+  hamburger.setAttribute("aria-label", isOpen ? "Close menu" : "Open menu");
+});
+
+// Zavřít menu kliknutím na odkaz
+mobileMenu.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("is-open");
+  });
+});
+
+console.log("test");
